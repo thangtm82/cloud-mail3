0b2107209691a86662301e420e899124291c927e
thangtm82 <thomastighe912@gmail.com>
2026-08-26 10:31:45 +0700

    feat: add signature fields to mailbox accounts

--- mail-worker/src/entity/account.js
+++ mail-worker/src/entity/account.js
@@ -10,6 +10,8 @@ export const account = sqliteTable('account', {
 	userId: integer('user_id').notNull(),
 	allReceive: integer('all_receive').default(0).notNull(),
 	sort: integer('sort').default(0).notNull(),
+	signature: text('signature').notNull().default(''),
+	signatureEnabled: integer('signature_enabled').default(0).notNull(),
 	isDel: integer('is_del').default(0).notNull(),
 });
 export default account
